160e0b1ec95d949db9d46c68f09bfa7ec4f2425e
Andrew Cutler <spyroninja@gmail.com>
2019-06-06 16:03:53 -0400

    only blunders which are captures are played

--- script.js
+++ script.js
@@ -1,38 +1,52 @@
 //chessjs engine
 var chess = new Chess()
+//set up initial board
+var board = ChessBoard("board", "start")
+//load blunders file and save to array
+var blunders
+
+//start game on click
+var startBtn = document.getElementById("start")
+startBtn.addEventListener("click", function() {
+  console.log(position)
+  //move to inside fetch?
+  //   newBlunderPosition = findBlunder(position)
+  loadBlunderPosition(newBlunderPosition)
+  console.log(newBlunderPosition.blunder)
+})
+
+//randomly loaded position
+var position
+//contains correct blunder move
+var newBlunderPosition
 
 //validate legal moves
 var movePiece = function(source, target) {
   if (chess.move({ from: source, to: target }) !== null) {
     chess.move({ from: source, to: target })
+    checkMove(chess.history().pop(), newBlunderPosition.blunder.move)
+    console.log(chess.history().pop())
+    //call move checker function
   } else return "snapback"
 }
 
-//set up initial board
-var board = ChessBoard("board", "start")
-
-//randomly loaded position
-var position
-
-//load blunders file and save to array
-var blunders
 fetch("blunders.txt")
   .then(res => res.text())
   .then(data => {
     blunders = data
     blunders = blunders.split("\n")
-    //choose random game
-    position = blunders[Math.floor(Math.random() * blunders.length)]
+    //choose random game where blunder is a capture
+    var validBlunder = false
+    grabBlunder()
+    newBlunderPosition = findBlunder(position)
+    while (!validBlunder) {
+      if (newBlunderPosition.blunder.move.indexOf("x") === -1) {
+        grabBlunder()
+        newBlunderPosition = findBlunder(position)
+      } else validBlunder = true
+    }
+    console.log(position, newBlunderPosition.blunder)
   })
-
-//start game
-var startBtn = document.getElementById("start")
-startBtn.addEventListener("click", function() {
-  console.log(position)
-  var newBlunderPosition = findBlunder(position)
-  loadBlunderPosition(newBlunderPosition)
-  console.log(newBlunderPosition.blunder)
-})
 
 // functions to find blunders and load position
 function findBlunder(positionPGN) {
@@ -74,4 +88,15 @@ function loadBlunderPosition(loadedBlunder) {
     onDrop: movePiece
   }
   board = ChessBoard("board", config)
+}
+
+//check if correct blunder is made
+function checkMove(playerMove, correctMove) {
+  if (playerMove === correctMove) console.log("correct")
+}
+
+//pull random blunder from blunders
+function grabBlunder() {
+  position = blunders[Math.floor(Math.random() * blunders.length)]
+  return position
 }
